8b023ebeea7b8034f0557c1cdc67d57ef7d96b09
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2022-01-26 10:13:37 -0600

    Update disk_usage.sh

--- disk_usage.sh
+++ disk_usage.sh
@@ -3,7 +3,7 @@
 
 Date = `date +% F" "% H:% M`
 IP = `ifconfig eth0 | awk '/inet addr/{print $ 2}' | cut -d: -f2`
-Total = `fdisk -l | grep" Disk/dev/sd [az] "| awk '{print $ 2 $ 3" GB "}' | sed 's/:/=/' | xargs echo -n | sed 's/[]/,/g'` # newline removed, separated by a comma in the message displays the total size of each partition
+Total = `fdisk -l | grep " Disk/dev/sd [az] "| awk '{print $ 2 $ 3" GB "}' | sed 's/:/=/' | xargs echo -n | sed 's/[]/,/g'` # newline removed, separated by a comma in the message displays the total size of each partition
 Disk_Use = `df -h | awk '{print $ 1" = "$ 5}' | sed '1d' | sed 's/%//g'`
 for i in $ Disk_Use
 do
