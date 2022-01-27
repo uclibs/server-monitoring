3cd6e3b0a57ec961b527f92d873a93eeb401246e
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2022-01-27 17:54:17 -0600

    Update disk_usage.sh

--- disk_usage.sh
+++ disk_usage.sh
@@ -1,15 +1,52 @@
+#!/usr/bin/env bash
 
-#!/Bin/bash
+# vars #
+disk="/dev/mapper/rhel-root" # disk to monitor
+current_usage=$(df -h | grep ${disk} | awk {'print $5'}) # get disk usage from monitored disk
+max_usage="83%" # max 83% disk usage
+mail="hortongn@ucmail.uc.edu" # mail to sent alert to
 
-Date = `date +% F" "% H:% M`
-IP = `ifconfig eth0 | awk '/inet addr/{print $ 2}' | cut -d: -f2`
-Total = `fdisk -l | grep " Disk/dev/sd [az] "| awk '{print $ 2 $ 3" GB "}' | sed 's/:/=/' | xargs echo -n | sed 's/[]/,/g'` # newline removed, separated by a comma in the message displays the total size of each partition
-Disk_Use = `df -h | awk '{print $ 1" = "$ 5}' | sed '1d' | sed 's/%//g'`
-for i in $ Disk_Use
-do
-        A = `echo $ i | awk -F '=' '{print $ 2}'`
-        if [$ A -gt 8]; then
-                echo -e "Date: $ Date\nHost: $ IP\nTotal: $ Total\nProblem: Part Use $ {i}%" | mail -s "Disk Monitor "  olanreaz@ucmail.uc.edu
-       
- fi
-done
+# functions #
+function max_exceeded() {
+
+    # tell that mail is being sent
+    echo "Max usage (${max_usage}) exceeded. Your disk usage is at ${current_usage}. Trying to send mail-alert..."
+
+    # check if the mail program exist
+    type mail > /dev/null 2>&1 || { 
+        echo >&2 "Mail does not exist. Install it and run script again. Aborting script..."; exit; 
+    }
+
+    # if the mail program exist we continue to this and send the alert
+    mailbody="Max usage (${max_usage}) exceeded. Your disk (${disk}) usage is at ${current_usage}."
+    echo ${mailbody} | mail -s "Disk alert!" "${mail}"
+
+    echo "Mail was sent to ${mail}"
+}
+
+function no_problems() {
+
+    echo "No problems. Disk (${disk}) usage at ${current_usage}. Max is set to ${max_usage}."
+}
+
+function main() {
+
+    # check if a valid disk is chosen
+    if [ ${current_usage} ]; then
+        # check if current disk usage is greater than or equal to max usage.
+        if [ ${current_usage%?} -ge ${max_usage%?} ]; then 
+            # if it is greater than or equal to max usage we call our max_exceeded function and send mail
+            max_exceeded
+        else
+            # if it is ok we do nothing
+            no_problems
+        fi
+    else
+        # if the disk is not valid, print valid disks on system
+        echo "Set a valid disk, and run script again. Your disks:"
+        df -h
+    fi
+}
+
+# init #
+main
