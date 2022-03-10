f42971a6cc4f78039cad3afaa6be7e6093b37b0e
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2022-03-10 12:08:15 -0600

    Update mem.sh

--- mem.sh
+++ mem.sh
@@ -1,29 +1,22 @@
-#!/bin/bash
-# Shell script to monitor or watch the high Mem-load
-# It will send an email to ADMIN, if the (memroy load is in %) percentage of Mem-load is >= 80%
+#!/bin/sh
+ramusage=$(free | awk '/Mem/{printf("RAM Usage: %.2f\n"), $3/$2*100}'| awk '{print $3}')
 
-# you don't need this, $HOSTNAME is a system variable and
-## already set.
-#HOSTNAME=`hostname`
+if [ "$ramusage" > 20 ]; then
 
-#Use lowercase variable names to avoid name collision with system variables. 
-load=80
-mailer=/bin/mail
-mailto="olanreaz@ucmail.uc.edu, hortongn@ucmail.uc.edu "
-
-if free -t | awk -vm="$load" 'NR == 2 { if($3/$2*100 > m){exit 0}else{exit 1}}'; then
- 
-  cores=$(grep -c processor /proc/cpuinfo)
-  cpuPerc=$(ps -eo pcpu= | awk -vcores=$cores '{k+=$1}END{printf "%.2f", k/cores}')
-  ## Get the more actual value for reporting
-  memPerc=$(free -t | awk 'FNR == 2 {printf("%.2f%"), $3/$2*100}')
-
-  message=$(cat <<EoF
-Please check your processess on $HOSTNAME the value of cpu load is $cpuPerc & Current Memory Utilization is: $memPerc.
-$(ps axo %mem,pid,euser,cmd | sort -nr | head -n 10)
-EoF
-         )
-  printf '%s\n' "$message" | 
-    "$mailer" -s "Memory Utilization is High > $load%, $memPerc % on $HOSTNAME" \
-     "$mailto"
+ SUBJECT="ATTENTION: Memory Utilization is High on $(hostname) at $(date)"
+ MESSAGE="/tmp/Mail.out"
+ TO="olanreaz@ucmail.uc.edu, hortongn@ucmail.uc.edu"
+ echo "Memory Current Usage is: $ramusage%" >> $MESSAGE
+ echo "" >> $MESSAGE
+ echo "------------------------------------------------------------------" >> $MESSAGE
+ echo "Top Memory Consuming Process Using top command" >> $MESSAGE
+ echo "------------------------------------------------------------------" >> $MESSAGE
+ echo "$(top -b -o +%MEM | head -n 20)" >> $MESSAGE
+ echo "" >> $MESSAGE
+ echo "------------------------------------------------------------------" >> $MESSAGE
+ echo "Top Memory Consuming Process Using ps command" >> $MESSAGE
+ echo "------------------------------------------------------------------" >> $MESSAGE
+ echo "$(ps -eo pid,ppid,%mem,%Memory,cmd --sort=-%mem | head)" >> $MESSAGE
+ mail -s "$SUBJECT" "$TO" < $MESSAGE
+ rm /tmp/Mail.out
 fi
